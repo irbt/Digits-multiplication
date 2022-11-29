4ffc9b4c18dba9e94df4d6d91e2ab0edaaf7b82b
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-11-29 21:37:11 +0100

    works

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Given a positive integer, write a method that returns multiplication of all digits in the number.
+namespace Program{
+    class DigitsTask{
+        public static void Main(string [] args){
+            Console.Clear();
+            static int DigitsMultiplication(int number){
+                
+                return  number>10 ? number % 10 * DigitsMultiplication (number/10) : number % 10;
+            }
+        Console.WriteLine (DigitsMultiplication(456));// → 120
+        Console.WriteLine (DigitsMultiplication(1));// → 6
+        }
+    }
+}
